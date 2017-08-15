9dc0821c3f137c266b34f2ac0682dd29e3976a6d
Christopher Charbonneau Wells <chris@chrxs.net>
2017-08-15 16:24:55 -0400

    Add basic Baby model forms.

--- core/urls.py
+++ core/urls.py
@@ -6,5 +6,10 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.IndexView.as_view(), name='index'),
+    url(r'^$', views.Index.as_view(), name='index'),
+    url(r'baby/add/$', views.BabyAdd.as_view(), name='baby-add'),
+    url(r'baby/(?P<pk>[0-9]+)/$', views.BabyUpdate.as_view(),
+        name='baby-update'),
+    url(r'baby/(?P<pk>[0-9]+)/delete/$', views.BabyDelete.as_view(),
+        name='baby-delete'),
 ]
--- core/views.py
+++ core/views.py
@@ -2,7 +2,26 @@
 from __future__ import unicode_literals
 
 from django.views.generic.base import TemplateView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+
+from .models import Baby
 
 
-class IndexView(TemplateView):
+class Index(TemplateView):
     template_name = 'core/index.html'
+
+
+class BabyAdd(CreateView):
+    model = Baby
+    fields = ['first_name', 'last_name', 'birth_date']
+    success_url = '/'
+
+
+class BabyUpdate(UpdateView):
+    model = Baby
+    fields = ['first_name', 'last_name', 'birth_date']
+    success_url = '/'
+
+
+class BabyDelete(DeleteView):
+    model = Baby
