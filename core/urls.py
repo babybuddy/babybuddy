8d9eef29157c457b499cac9c6fa1472a7d1e3732
Christopher Charbonneau Wells <chris@chrxs.net>
2017-08-16 09:12:49 -0400

    Add basic Child list page example.

--- core/urls.py
+++ core/urls.py
@@ -7,9 +7,11 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.Index.as_view(), name='index'),
-    url(r'child/add/$', views.ChildAdd.as_view(), name='child-add'),
-    url(r'child/(?P<pk>[0-9]+)/$', views.ChildUpdate.as_view(),
+
+    url(r'children/$', views.ChildList.as_view(), name='child-list'),
+    url(r'children/add/$', views.ChildAdd.as_view(), name='child-add'),
+    url(r'children/(?P<pk>[0-9]+)/$', views.ChildUpdate.as_view(),
         name='child-update'),
-    url(r'child/(?P<pk>[0-9]+)/delete/$', views.ChildDelete.as_view(),
+    url(r'children/(?P<pk>[0-9]+)/delete/$', views.ChildDelete.as_view(),
         name='child-delete'),
 ]
--- core/views.py
+++ core/views.py
@@ -3,6 +3,7 @@ from __future__ import unicode_literals
 
 from django.views.generic.base import TemplateView
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.views.generic.list import ListView
 
 from .models import Child
 
@@ -11,16 +12,20 @@ class Index(TemplateView):
     template_name = 'core/index.html'
 
 
+class ChildList(ListView):
+    model = Child
+
+
 class ChildAdd(CreateView):
     model = Child
     fields = ['first_name', 'last_name', 'birth_date']
-    success_url = '/'
+    success_url = '/children'
 
 
 class ChildUpdate(UpdateView):
     model = Child
     fields = ['first_name', 'last_name', 'birth_date']
-    success_url = '/'
+    success_url = '/children'
 
 
 class ChildDelete(DeleteView):
