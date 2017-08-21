c0b38f3790fa23e0655fc83f6a10e6040de3ec0c
Christopher Charbonneau Wells <chris@chrxs.net>
2017-08-21 16:35:11 -0400

    Add the watch task to gulp's default task.

--- gulpfile.js/tasks/default.js
+++ gulpfile.js/tasks/default.js
@@ -1,4 +1,4 @@
 const gulp = require('gulp');
 
 
-gulp.task('default', ['scripts', 'styles', 'extras']);
+gulp.task('default', ['scripts', 'styles', 'extras', 'watch']);
--- gulpfile.js/tasks/watch.js
+++ gulpfile.js/tasks/watch.js
@@ -6,10 +6,14 @@ gulp.task('watch', ['watch:scripts', 'watch:styles']);
 gulp.task('watch:scripts', function() {
     return gulp.watch([
         '**/static/js/**/*.js',
+        'babyblotter/static_site/js/**/*.js',
         '!babyblotter/static/js/'
     ], ['scripts:app']);
 });
 
 gulp.task('watch:styles', function() {
-    return gulp.watch('**/static/scss/**/*.scss', ['styles:app']);
+    return gulp.watch([
+        '**/static/scss/**/*.scss',
+        'babyblotter/static_site/scss/**/*.scss'
+    ], ['styles:app']);
 });
