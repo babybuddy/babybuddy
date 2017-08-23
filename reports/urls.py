ed96814b103991dfe83cf4e245c7acb6d42a32e8
Christopher Charbonneau Wells <chris@chrxs.net>
2017-08-23 09:04:05 -0400

    Revert custom graph works and add Plotly-based example.

--- reports/urls.py
+++ reports/urls.py
@@ -6,6 +6,8 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
+    url(r'^reports/(?P<slug>[^/.]+)/changes/$',
+        views.DiaperChangeReport.as_view(), name='report-diaperchange'),
     url(r'^reports/(?P<slug>[^/.]+)/sleep/$',
         views.SleepReport.as_view(), name='report-sleep'),
 ]
--- reports/views.py
+++ reports/views.py
@@ -1,13 +1,50 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
-from collections import OrderedDict
-
 from django.contrib.auth.mixins import PermissionRequiredMixin
-from django.utils import timezone
+from django.db.models import Count
+from django.db.models.functions import TruncDate
 from django.views.generic.detail import DetailView
 
-from core.models import Child, Sleep
+import plotly.offline as plotly
+import plotly.graph_objs as go
+
+from core.models import Child, DiaperChange
+
+
+class DiaperChangeReport(PermissionRequiredMixin, DetailView):
+    """All sleep data for a child."""
+    model = Child
+    permission_required = ('core.view_child',)
+    template_name = 'reports/diaperchange.html'
+
+    def get_context_data(self, **kwargs):
+        context = super(DiaperChangeReport, self).get_context_data(**kwargs)
+        child = context['object']
+
+        # TODO: Move this logic to the model? Where should data processing like
+        # this happen?
+        changes = DiaperChange.objects.filter(child=child)\
+            .annotate(date=TruncDate('time'))\
+            .values('date')\
+            .annotate(count=Count('id'))\
+            .order_by('-date')
+
+        trace1 = go.Scatter(
+            x=list(changes.values_list('date', flat=True)),
+            y=list(changes.values_list('count', flat=True))
+        )
+        fig = go.Figure({
+            "data": [trace1],
+            "layout": go.Layout(title='Diaper Changes')
+        })
+        div = plotly.plot(fig, output_type='div', include_plotlyjs=False)
+        html, javascript = div.split('<script')
+
+        context['html'] = html
+        context['javascript'] = '<script' + javascript
+
+        return context
 
 
 class SleepReport(PermissionRequiredMixin, DetailView):
@@ -18,30 +55,4 @@ class SleepReport(PermissionRequiredMixin, DetailView):
 
     def get_context_data(self, **kwargs):
         context = super(SleepReport, self).get_context_data(**kwargs)
-        child = context['object']
-
-        sleep_entries = Sleep.objects.filter(child=child).order_by('-end')
-        diff = timezone.localtime().date() - timezone.localtime(sleep_entries.last().end).date()
-        stats = OrderedDict()
-        for x in range(0, diff.days + 1):
-            key = (timezone.localtime() - timezone.timedelta(days=x))
-            stats[key.date()] = []
-
-        last = None
-        for entry in sleep_entries:
-            start = timezone.localtime(entry.start)
-            end = timezone.localtime(entry.end)
-
-            if not stats[end.date()]:
-                last = end.replace(hour=23, minute=59, second=59)
-
-            # TODO: Account for sleep sessions crossing midnight.
-            stats[start.date()].append((
-                (last - end).seconds/86400 * 100,
-                (end - start).seconds/86400 * 100
-            ))
-            last = start
-
-        context['stats'] = stats
-
         return context
