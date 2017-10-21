ba1ab676ee53e84665214101082683fac02a93c3
Christopher Charbonneau Wells <chris@chrxs.net>
2017-10-21 17:36:44 -0400

    Add simple default pagination to API endpoints.

--- babyblotter/settings/base.py
+++ babyblotter/settings/base.py
@@ -118,5 +118,8 @@ REST_FRAMEWORK = {
     ],
     'DEFAULT_RENDERER_CLASSES': (
         'rest_framework.renderers.JSONRenderer',
-    )
+    ),
+    'DEFAULT_PAGINATION_CLASS':
+        'rest_framework.pagination.LimitOffsetPagination',
+    'PAGE_SIZE': 100
 }
