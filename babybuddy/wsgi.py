b28bb0cd56f28c72ab1e66e82929b9e8619b4abd
Christopher Charbonneau Wells <chris@chrxs.net>
2017-10-23 05:19:32 -0400

    Use development settings for wsgi for now.

--- babybuddy/wsgi.py
+++ babybuddy/wsgi.py
@@ -3,6 +3,6 @@ import os
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault(
-    "DJANGO_SETTINGS_MODULE", "babybuddy.settings.production")
+    "DJANGO_SETTINGS_MODULE", "babybuddy.settings.development")
 
 application = get_wsgi_application()
