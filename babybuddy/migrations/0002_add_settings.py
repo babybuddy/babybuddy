6ba590092846ca240629091df2f064b389d15eca
Christopher Charbonneau Wells <chris@chrxs.net>
2017-11-12 20:15:11 -0500

    Add migration to handle existing users when adding the Settings model.

---
+++ babybuddy/migrations/0002_add_settings.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import migrations
+
+
+def add_settings(apps, schema_editor):
+    Settings = apps.get_model('babybuddy', 'Settings')
+    User = apps.get_model('auth', 'User')
+    for user in User.objects.all():
+        if Settings.objects.filter(user=user).count() == 0:
+            settings = Settings.objects.create(user=user)
+            settings.save()
+
+
+class Migration(migrations.Migration):
+
+    initial = True
+
+    dependencies = [
+        ('babybuddy', '0001_initial'),
+    ]
+
+    operations = [
+        migrations.RunPython(add_settings, reverse_code=migrations.RunPython.noop),
+    ]
