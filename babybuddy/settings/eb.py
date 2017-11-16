65c46351cb45c186333e7d4b53aa09b1ddb3c106
Christopher Charbonneau Wells <chris@chrxs.net>
2017-11-16 10:57:49 -0500

    Add Amazon EB config files.

---
+++ babybuddy/settings/eb.py
@@ -0,0 +1,26 @@
+import os
+
+from .base import *  # noqa: F401,F403
+
+
+DEBUG = os.environ.get('DEBUG', False)
+
+
+# SECURITY WARNING: keep the secret key used in production secret!
+
+SECRET_KEY = os.environ['SECRET_KEY']
+
+
+# Database
+# https://docs.djangoproject.com/en/1.11/ref/settings/#databases
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': os.environ['RDS_DB_NAME'],
+        'USER': os.environ['RDS_USERNAME'],
+        'PASSWORD': os.environ['RDS_PASSWORD'],
+        'HOST': os.environ['RDS_HOSTNAME'],
+        'PORT': os.environ['RDS_PORT'],
+    }
+}
