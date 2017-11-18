c7d09823bb75146602146606823f0f7906edbec3
Isaac Bythewood <isaac@bythewood.me>
2017-11-18 04:29:46 -0500

    Fix linting issue with url line length

--- babybuddy/urls.py
+++ babybuddy/urls.py
@@ -29,4 +29,7 @@ urlpatterns = [
 ]
 
 if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns += static(
+        settings.MEDIA_URL,
+        document_root=settings.MEDIA_ROOT
+    )
