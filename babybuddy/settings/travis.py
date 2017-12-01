2cd3aacfbfeb279d5e51bfbf0023c3138d9b9c6d
Christopher Charbonneau Wells <chris@chrxs.net>
2017-11-30 23:10:08 -0500

    Add Travis settings file.

---
+++ babybuddy/settings/travis.py
@@ -0,0 +1,14 @@
+from .base import *
+
+SECRET_KEY = 'TRAVISCIWHYNOT'
+
+
+# Database
+# https://docs.djangoproject.com/en/1.11/ref/settings/#databases
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
