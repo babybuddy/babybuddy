c37dabdbd18758c6358fbfb68a9396683e826ddb
Christopher Charbonneau Wells <chris@chrxs.net>
2017-12-05 16:40:10 -0500

    Disable unused API methods and require model add permissions for OPTIONS.

--- api/permissions.py
+++ api/permissions.py
@@ -7,10 +7,10 @@ from rest_framework.permissions import DjangoModelPermissions
 class BabyBuddyDjangoModelPermissions(DjangoModelPermissions):
     perms_map = {
         'GET': ['%(app_label)s.view_%(model_name)s'],
-        'OPTIONS': [],
+        'OPTIONS': ['%(app_label)s.add_%(model_name)s'],
         'HEAD': [],
         'POST': ['%(app_label)s.add_%(model_name)s'],
-        'PUT': ['%(app_label)s.change_%(model_name)s'],
-        'PATCH': ['%(app_label)s.change_%(model_name)s'],
-        'DELETE': ['%(app_label)s.delete_%(model_name)s'],
+        # 'PUT': ['%(app_label)s.change_%(model_name)s'],
+        # 'PATCH': ['%(app_label)s.change_%(model_name)s'],
+        # 'DELETE': ['%(app_label)s.delete_%(model_name)s'],
     }
