26b54b114e3e2408fb34a8b642cbcb232f20c2bc
Christopher C. Wells <chris@chrxs.net>
2020-01-27 14:56:37 -0800

    Add wrapper for datetime picker widget

--- static/babybuddy/js/app.js
+++ static/babybuddy/js/app.js
@@ -1,15 +1,39 @@
-/* Baby Buddy
- *
- * Default namespace for the Baby Buddy app.
- */
 if (typeof jQuery === 'undefined') {
   throw new Error('Baby Buddy requires jQuery.')
 }
+if (typeof moment === 'undefined') {
+  throw new Error('Baby Buddy requires moment.js.')
+}
 
+/**
+ * Baby Buddy Namespace
+ *
+ * Default namespace for the Baby Buddy app.
+ */
 var BabyBuddy = function () {
-    var BabyBuddy = {};
-    return BabyBuddy;
+    return {};
 }();
+
+/**
+ * Baby Buddy Datetime Picker
+ *
+ * Provides modifications and defaults for the base datetime picker widget.
+ */
+BabyBuddy.DatetimePicker = function ($, moment) {
+    return {
+        init: function (element, options) {
+            var defaultOptions = {
+                buttons: { showToday: true },
+                defaultDate: 'now',
+                format: 'L LT',
+                ignoreReadonly: true,
+                locale: moment.locale(),
+                useCurrent: false
+            };
+            element.datetimepicker($.extend(defaultOptions, options));
+        }
+    };
+}(jQuery, moment);
 
 /* Baby Buddy Timer
  *
