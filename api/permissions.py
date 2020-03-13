0082f0d8386dd26d0009bc0d08c4ff19ddc2bd9f
Christopher C. Wells <chris@chrxs.net>
2020-03-13 06:29:14 -0700

    Add PATCH and DELETE support (#126)

--- api/permissions.py
+++ api/permissions.py
@@ -9,6 +9,6 @@ class BabyBuddyDjangoModelPermissions(DjangoModelPermissions):
         'HEAD': [],
         'POST': ['%(app_label)s.add_%(model_name)s'],
         # 'PUT': ['%(app_label)s.change_%(model_name)s'],
-        # 'PATCH': ['%(app_label)s.change_%(model_name)s'],
-        # 'DELETE': ['%(app_label)s.delete_%(model_name)s'],
+        'PATCH': ['%(app_label)s.change_%(model_name)s'],
+        'DELETE': ['%(app_label)s.delete_%(model_name)s'],
     }
