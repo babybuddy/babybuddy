3407afbf1b52887db67765ef1e996b3d3c1ce6e3
Christopher C. Wells <chris@chrxs.net>
2020-06-21 08:08:59 -0700

    Move default database file to a directory (#145)

--- babybuddy/settings/base.py
+++ babybuddy/settings/base.py
@@ -101,7 +101,7 @@ TEMPLATES = [
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'NAME': os.path.join(BASE_DIR, 'data/db.sqlite3'),
     }
 }
 
