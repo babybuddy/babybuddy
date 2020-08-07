22cae99e661a734f29ac010a18f35514c8b36f39
Stephen Stratoti <stratoti@gmail.com>
2020-08-07 13:30:09 -0400

    Updates babybuddy to use MySQL as a backend database.

---
+++ babybuddy/settings/mysql.py
@@ -0,0 +1,13 @@
+from .base import *
+# Database
+# https://docs.djangoproject.com/en/3.1/ref/settings/#databases
+# https://docs.djangoproject.com/en/3.1/ref/databases/#mysql-notes
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'OPTIONS': {
+            'read_default_file': '/app/my.cnf',
+        },
+    }
+}
