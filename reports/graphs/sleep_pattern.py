9228d249caae8485551cb570ab40dd8873e08ddd
Christopher C. Wells <git@chris-wells.net>
2020-12-02 16:26:14 -0800

    Remove dataframes code from sleep pattern graph

--- reports/graphs/sleep_pattern.py
+++ reports/graphs/sleep_pattern.py
@@ -4,7 +4,6 @@ from collections import OrderedDict
 from django.utils import timezone
 from django.utils.translation import gettext as _
 
-import pandas as pd
 import plotly.offline as plotly
 import plotly.graph_objs as go
 
@@ -21,12 +20,8 @@ def sleep_pattern(instances):
     """
     times = {}
     labels = {}
-
-    y_df = pd.DataFrame()
-    text_df = pd.DataFrame()
     last_end_time = None
     adjustment = None
-    df_index = 0
     for instance in instances:
         start_time = timezone.localtime(instance.start)
         end_time = timezone.localtime(instance.end)
@@ -41,24 +36,10 @@ def sleep_pattern(instances):
         # Check if the previous entry crossed midnight (see below).
         if adjustment:
             # Fake (0) entry to keep the color switching logic working.
-            df_index = _add_sleep_entry(
-                y_df, text_df, 0, adjustment['column'], 0)
             times[adjustment['column']].append(0)
             labels[adjustment['column']].append(0)
 
             # Real adjustment entry.
-            df_index = _add_sleep_entry(
-                y_df,
-                text_df,
-                df_index,
-                adjustment['column'],
-                adjustment['duration'].seconds/60,
-                'Asleep {} ({} to {})'.format(
-                    duration_string(adjustment['duration']),
-                    adjustment['start_time'].strftime('%I:%M %p'),
-                    adjustment['end_time'].strftime('%I:%M %p')
-                )
-            )
             times[adjustment['column']].append(
                 adjustment['duration'].seconds/60)
             labels[adjustment['column']].append('Asleep {} ({} to {})'.format(
@@ -85,36 +66,14 @@ def sleep_pattern(instances):
                 day=start_time.day, hour=23, minute=59, second=0)
             duration = end_time - start_time
 
-        if start_date not in y_df:
-            last_end_time = start_time.replace(hour=0, minute=0, second=0)
-        # TODO: Fix this -- doesn't appear to work in the extra same way.
-        if start_date not in times:
+        if not last_end_time:
             last_end_time = start_time.replace(hour=0, minute=0, second=0)
 
         # Awake time.
-        df_index = _add_sleep_entry(
-            y_df,
-            text_df,
-            df_index,
-            start_date,
-            (start_time - last_end_time).seconds/60
-        )
         times[start_date].append((start_time - last_end_time).seconds/60)
         labels[start_date].append(None)
 
         # Asleep time.
-        df_index = _add_sleep_entry(
-            y_df,
-            text_df,
-            df_index,
-            start_date,
-            duration.seconds/60,
-            'Asleep {} ({} to {})'.format(
-                duration_string(duration),
-                start_time.strftime('%I:%M %p'),
-                end_time.strftime('%I:%M %p')
-            )
-        )
         times[start_date].append(duration.seconds/60)
         labels[start_date].append('Asleep {} ({} to {})'.format(
             duration_string(duration),
@@ -197,25 +156,3 @@ def sleep_pattern(instances):
     })
     output = plotly.plot(fig, output_type='div', include_plotlyjs=False)
     return utils.split_graph_output(output)
-
-
-def _add_sleep_entry(y_df, text_df, index, column, duration, text=''):
-    """
-    Create a duration and text description entry in a DataFrame and return
-    the next index on success.
-    :param y_df: the y values DataFrame.
-    :param text_df: the text values DataFrame.
-    :param index: the index to target in both y_df and text_df.
-    :param column: the column (date) to make the entry in.
-    :param duration: the duration of the entry.
-    :param text: text to go with the entry (displays on graph hover).
-    :return: the next index of the DataFrames.
-    """
-    if column not in y_df:
-        y_df.assign(**{column: 0 in range(0, len(y_df.index))})
-        text_df.assign(**{column: 0 in range(0, len(text_df.index))})
-        index = 0
-
-    y_df.at[index, column] = duration
-    text_df.at[index, column] = text
-    return index + 1
