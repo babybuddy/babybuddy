a4afa27813db74b65eb6a5abd2f8ef28bb03295d
Michael Zufelt <mike.zufelt@gmail.com>
2021-01-03 12:55:11 -0500

    Updated docker.py to allow for custom PSQL env input

--- babybuddy/settings/docker.py
+++ babybuddy/settings/docker.py
@@ -4,13 +4,14 @@ from .base import *
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
+# Load settings from env file / variables with fallback defaults to support current psql deployment
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'postgres',
-        'USER': 'postgres',
+        'NAME': os.getenv('POSTGRES_DB') if os.getenv('POSTGRES_DB') else 'postgres',
+        'USER': os.getenv('POSTGRES_USER') if os.getenv('POSTGRES_USER') else 'postgres',
         'PASSWORD': os.environ.get('POSTGRES_PASSWORD'),
-        'HOST': 'db',
-        'PORT': 5432,
+        'HOST': os.getenv('POSTGRES_HOST') if os.getenv('POSTGRES_HOST') else 'db',
+        'PORT': os.getenv('POSTGRES_PORT') if os.getenv('POSTGRES_PORT') else 5432,
     }
 }
