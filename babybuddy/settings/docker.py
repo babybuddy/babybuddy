84024af1c05c396be65e0420fb1c6b1b9c8ab430
Michael Zufelt <mike.zufelt@gmail.com>
2021-01-04 19:18:51 -0500

    Updated database password fallback to prevent breaking existing deployments

--- babybuddy/settings/docker.py
+++ babybuddy/settings/docker.py
@@ -10,7 +10,7 @@ DATABASES = {
         'ENGINE': os.getenv('DB_ENGINE') or 'django.db.backends.postgresql_psycopg2',
         'NAME': os.getenv('DB_NAME') or 'postgres',
         'USER': os.getenv('DB_USER') or 'postgres',
-        'PASSWORD': os.environ.get('DB_PASSWORD'),
+        'PASSWORD': os.environ.get('DB_PASSWORD') or os.environ.get('POSTGRES_PASSWORD'),
         'HOST': os.getenv('DB_HOST') or 'db',
         'PORT': os.getenv('DB_PORT') or 5432,
     }
