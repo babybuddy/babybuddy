26fa988fe6f00a517e9dbc56d9009ed423c5f9dc
Christopher Charbonneau Wells <github@chris-wells.net>
2022-02-20 22:27:19 +0000

    Add Gitpod config for CSRF origin
    Supports CSRF with Django 4+ (Baby Buddy v1.10.0+).
    
    See #393.

---
+++ babybuddy/settings/gitpod.py
@@ -0,0 +1,9 @@
+from .development import *
+
+# CSRF configuration
+# https://docs.djangoproject.com/en/4.0/ref/settings/#std:setting-CSRF_TRUSTED_ORIGINS
+# https://www.gitpod.io/docs/environment-variables/#default-environment-variables
+
+CSRF_TRUSTED_ORIGINS = [
+    os.environ.get("GITPOD_WORKSPACE_URL").replace("https://", "https://8000-")
+]
