369c4d9bc6785aa880961fda0bb8ed4c1fd768d6
Christopher C. Wells <git-2022@chris-wells.net>
2022-05-22 14:04:03 -0700

    Create basic render.com configuration

---
+++ babybuddy/settings/render.py
@@ -0,0 +1,19 @@
+import dj_database_url
+
+from .base import *
+
+RENDER_EXTERNAL_HOSTNAME = os.environ.get('RENDER_EXTERNAL_HOSTNAME')
+if RENDER_EXTERNAL_HOSTNAME:
+    ALLOWED_HOSTS.append(RENDER_EXTERNAL_HOSTNAME)
+    CSRF_TRUSTED_ORIGINS.append(RENDER_EXTERNAL_HOSTNAME)
+
+
+# Database
+# https://docs.djangoproject.com/en/4.0/ref/settings/#databases
+
+DATABASES = {
+    'default': dj_database_url.config(
+        default='postgresql://postgres:postgres@localhost:5432/babybuddy',
+        conn_max_age=600
+    )
+}
