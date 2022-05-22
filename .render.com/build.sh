3d0b459849a980979163aeeb96402b37686e4d6a
Christopher C. Wells <git-2022@chris-wells.net>
2022-05-22 14:23:04 -0700

    Specify Python version in render config

--- .render.com/build.sh
+++ .render.com/build.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 set -o errexit
 
-python3 -m pip install --upgrade pip
-pip3 install -r requirements.txt
+python -m pip install --upgrade pip
+pip install -r requirements.txt
 
-python3 manage.py migrate
+python manage.py migrate
