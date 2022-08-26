e68c147fdb2d879864fe1fd3ebc059852e5a445d
Christopher C. Wells <git-2022@chris-wells.net>
2022-08-26 07:57:52 -0700

    Bind gunicorn 0.0.0.0:80

--- Procfile.sh
+++ Procfile.sh
@@ -3,4 +3,4 @@
 python manage.py migrate
 python manage.py createcachetable
 
-gunicorn babybuddy.wsgi:application --timeout 30 --log-file -
+gunicorn babybuddy.wsgi:application -b 0.0.0.0:80 --timeout 30 --log-file -
