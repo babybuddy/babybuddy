7424155ba4c666f65927f21c77f90343b2c5137b
Paul Konstantin Gerke <paulkgerke@craftware.info>
2022-08-30 21:34:04 +0200

    Remove unused imports

--- babybuddy/views.py
+++ babybuddy/views.py
@@ -1,8 +1,4 @@
 # -*- coding: utf-8 -*-
-import base64
-
-from urllib.parse import urljoin
-
 from django.contrib import messages
 from django.contrib.auth import update_session_auth_hash
 from django.contrib.auth.forms import PasswordChangeForm
