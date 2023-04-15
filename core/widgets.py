2007f998cf1face62258e2e6b577c993fe40ccac
Christopher C. Wells <git-2022@chris-wells.net>
2023-04-15 12:58:41 -0700

    Default datetime input to seconds granularity

--- core/widgets.py
+++ core/widgets.py
@@ -116,6 +116,13 @@ class DateTimeBaseInput(widgets.DateTimeBaseInput):
 class DateTimeInput(DateTimeBaseInput):
     input_type = "datetime-local"
 
+    def build_attrs(self, base_attrs, extra_attrs=None):
+        attrs = super().build_attrs(base_attrs, extra_attrs)
+        # Default to seconds granularity. Required for client validation in Safari.
+        if "step" not in attrs:
+            attrs["step"] = 1
+        return attrs
+
 
 class DateInput(DateTimeBaseInput):
     input_type = "date"
