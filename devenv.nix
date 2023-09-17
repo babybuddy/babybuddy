c59278351bd53942a3071ed6703c2f6a5fb979fa
Michael Salaverry <barakplasma@gmail.com>
2023-09-17 04:09:46 +0300

    feat: devenv support for reproducible developer environment using nix

---
+++ devenv.nix
@@ -0,0 +1,36 @@
+{ pkgs, ... }:
+
+{
+  # https://devenv.sh/basics/
+  env.DJANGO_SETTINGS_MODULE = "babybuddy.settings.development";
+
+  # https://devenv.sh/packages/
+  packages = [
+    pkgs.pipenv
+    pkgs.python311
+    pkgs.nodejs_18
+    pkgs.glib
+  ];
+
+  # https://devenv.sh/scripts/
+
+  enterShell = ''
+    pipenv install --dev
+    npm i
+    npx gulp migrate
+    npx gulp
+  '';
+
+  # https://devenv.sh/languages/
+  languages.python.enable = true;
+  languages.javascript = {
+    enable = true;
+    npm.install.enable = true;
+  };
+
+  devcontainer.enable = true;
+
+  services.nginx.enable = true;
+
+  # See full reference at https://devenv.sh/reference/options/
+}
