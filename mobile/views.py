b8e8f020b377b0bd18097f35ab93958be6e25fe9
billybonks <sebastienstettler@gmail.com>
2024-01-24 15:46:54 +0800

    wipidoo

--- mobile/views.py
+++ mobile/views.py
@@ -1,3 +1,5 @@
+from typing import Any
+from django.http import HttpRequest, HttpResponse
 from django.views.generic.detail import DetailView
 from django.views.generic.edit import CreateView, UpdateView, DeleteView, FormView
 from babybuddy.mixins import PermissionRequiredMixin
@@ -5,6 +7,9 @@ from core.models import Child, DiaperChange
 from django import forms
 from django.urls import reverse, reverse_lazy
 from mobile.constants import activities
+from datetime import datetime
+from django.utils import timezone
+from babybuddy.widgets import DateInput, DateTimeInput, TimeInput
 
 
 class MobileChildDashboard(PermissionRequiredMixin, DetailView):
@@ -25,10 +30,20 @@ class CoreAddView(PermissionRequiredMixin, CreateView):
     def get_context_data(self, **kwargs):
         # Call the base implementation first to get a context
         context = super().get_context_data(**kwargs)
-        child = Child.objects.filter(slug__exact=context["view"].kwargs["slug"])[0]
-        context["child"] = child
         context["theme"] = activities[context["form"].theme]
         return context
+
+    def get_initial(self):
+        initial = super().get_initial()
+        child = Child.objects.filter(slug__exact=self.kwargs["slug"])[0]
+        initial["child"] = child
+        initial["time"] = timezone.now()
+        print(initial)
+        return initial
+
+    def post(self, request: HttpRequest, *args: str, **kwargs: Any) -> HttpResponse:
+        print(self.get_form())
+        return super().post(request, *args, **kwargs)
 
 
 class DiaperChangeForm(forms.ModelForm):
@@ -37,6 +52,9 @@ class DiaperChangeForm(forms.ModelForm):
     class Meta:
         model = DiaperChange
         fields = ["time"]
+        widgets = {
+            "time": DateTimeInput(),
+        }
 
 
 class DiaperChangeAdd(CoreAddView):
