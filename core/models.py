7bafccf8923a3f62da0d19afd073fe61ba36ab08
Justin Goette <53531335+jcgoette@users.noreply.github.com>
2025-08-11 13:20:26 -0400

    fix: Order date only models on id if multiple entries on same date

--- core/models.py
+++ core/models.py
@@ -154,7 +154,7 @@ class BMI(models.Model):
 
     class Meta:
         default_permissions = ("view", "add", "change", "delete")
-        ordering = ["-date"]
+        ordering = ["-date", "-id"]
         verbose_name = _("BMI")
         verbose_name_plural = _("BMI")
 
@@ -369,7 +369,7 @@ class HeadCircumference(models.Model):
 
     class Meta:
         default_permissions = ("view", "add", "change", "delete")
-        ordering = ["-date"]
+        ordering = ["-date", "-id"]
         verbose_name = _("Head Circumference")
         verbose_name_plural = _("Head Circumference")
 
@@ -399,7 +399,7 @@ class Height(models.Model):
 
     class Meta:
         default_permissions = ("view", "add", "change", "delete")
-        ordering = ["-date"]
+        ordering = ["-date", "-id"]
         verbose_name = _("Height")
         verbose_name_plural = _("Height")
 
@@ -733,7 +733,7 @@ class Weight(models.Model):
 
     class Meta:
         default_permissions = ("view", "add", "change", "delete")
-        ordering = ["-date"]
+        ordering = ["-date", "-id"]
         verbose_name = _("Weight")
         verbose_name_plural = _("Weight")
 
