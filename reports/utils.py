16cd51e6ed23157fb756ec4f05b1c1cc8013bdc3
Laurens <laurens@shockmedia.nl>
2025-08-26 17:44:07 +0200

    Revert

--- reports/utils.py
+++ reports/utils.py
@@ -58,11 +58,11 @@ def rangeselector_date():
         "bgcolor": "rgb(35, 149, 86)",
         "activecolor": "rgb(25, 108, 62)",
         "buttons": [
-            {"count": 7, "label": '{% trans "graph_1w" %}', "step": "day", "stepmode": "backward"},
-            {"count": 14, "label": '{% trans "graph_2w" %}', "step": "day", "stepmode": "backward"},
-            {"count": 1, "label": '{% trans "graph_1m" %}', "step": "month", "stepmode": "backward"},
-            {"count": 3, "label": '{% trans "graph_3m" %}', "step": "month", "stepmode": "backward"},
-            {"step": '{% trans "graph_all" %}'},
+            {"count": 7, "label": '{% trans "1w" %}', "step": "day", "stepmode": "backward"},
+            {"count": 14, "label": '{% trans "2w" %}', "step": "day", "stepmode": "backward"},
+            {"count": 1, "label": '{% trans "1m" %}', "step": "month", "stepmode": "backward"},
+            {"count": 3, "label": '{% trans "3m" %}', "step": "month", "stepmode": "backward"},
+            {"step": '{% trans "all" %}'},
         ],
     }
 
