676bf345f8e7b307955c06f6b70bd1aa70d1a224
Florin Vlaicu <19238716+fvlaicu@users.noreply.github.com>
2026-05-25 22:21:10 +0300

    Setting this as None makes it use upstream

--- babybuddy/settings/base.py
+++ babybuddy/settings/base.py
@@ -377,29 +377,7 @@ AXES_LOCKOUT_TEMPLATE = "error/lockout.html"
 
 AXES_LOCKOUT_URL = "/login/lock"
 
-AXES_IPWARE_META_PRECEDENCE_ORDER = (
-    "X_FORWARDED_FOR",
-    # Load balancers or proxies such as AWS ELB (default client is `left-most` [`<client>, <proxy1>, <proxy2>`])
-    "HTTP_X_FORWARDED_FOR",  # Similar to X_FORWARDED_TO
-    "HTTP_CLIENT_IP",  # Standard headers used by providers such as Amazon EC2, Heroku etc.
-    "HTTP_X_REAL_IP",  # Standard headers used by providers such as Amazon EC2, Heroku etc.
-    "HTTP_X_FORWARDED",  # Squid and others
-    "HTTP_X_CLUSTER_CLIENT_IP",  # Rackspace LB and Riverbed Stingray
-    "HTTP_FORWARDED_FOR",  # RFC 7239
-    "HTTP_FORWARDED",  # RFC 7239
-    "HTTP_CF_CONNECTING_IP",  # CloudFlare
-    "X-CLIENT-IP",  # Microsoft Azure
-    "X-REAL-IP",  # NGINX
-    "X-CLUSTER-CLIENT-IP",  # Rackspace Cloud Load Balancers
-    "X_FORWARDED",  # Squid
-    "FORWARDED_FOR",  # RFC 7239
-    "CF-CONNECTING-IP",  # CloudFlare
-    "TRUE-CLIENT-IP",  # CloudFlare Enterprise,
-    "FASTLY-CLIENT-IP",  # Firebase, Fastly
-    "FORWARDED",  # RFC 7239
-    "CLIENT-IP",  # Akamai and Cloudflare: True-Client-IP and Fastly: Fastly-Client-IP
-    "REMOTE_ADDR",  # Default
-)
+AXES_IPWARE_META_PRECEDENCE_ORDER = None
 
 # Session configuration
 # Used by RollingSessionMiddleware to determine how often to reset the session.
