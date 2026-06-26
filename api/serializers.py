a7f66f9f475a69ceca26ad9090bc89aec1237ba4
Pietro Falcone <pietro.falcone@gmail.com>
2026-06-26 18:06:53 +0100

    feat: plot growth percentiles by corrected age for preterm children

--- api/serializers.py
+++ api/serializers.py
@@ -164,6 +164,7 @@ class ChildSerializer(serializers.HyperlinkedModelSerializer):
             "last_name",
             "birth_date",
             "birth_time",
+            "due_date",
             "slug",
             "picture",
         )
